0922cadde67cdfb29ee1dd00bf6954e4ce22e78d
Jeff Lindsay <progrium@gmail.com>
2017-02-14 18:30:43 -0600

    fixing a type led to general cleanup. also added a comment.
    Signed-off-by: Jeff Lindsay <progrium@gmail.com>

--- _example/public_key/public_key.go
+++ _example/public_key/public_key.go
@@ -1,33 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 
-	b64 "encoding/base64"
-
 	"github.com/gliderlabs/ssh"
+	gossh "golang.org/x/crypto/ssh"
 )
 
 func main() {
-
 	ssh.Handle(func(s ssh.Session) {
-		user := s.User()
-		keyType := s.PublicKey().Type()
-
-		publicKeyString := keyType + " " + b64.StdEncoding.EncodeToString(s.PublicKey().Marshal())
-
-		io.WriteString(s, "Hello "+user+"\n\n")
-		io.WriteString(s, "your public key:\n")
-		io.WriteString(s, publicKeyString+"\n\n")
+		authorizedKey := gossh.MarshalAuthorizedKey(s.PublicKey())
+		io.WriteString(s, fmt.Sprintf("public key used by %s:\n", s.User()))
+		s.Write(authorizedKey)
 	})
 
-	publicKeyHandler := ssh.PublicKeyAuth(func(user string, key ssh.PublicKey) bool {
-		// allow all keys
-		// use ssh.KeysEqual() to compare agains know keys
-		return true
+	publicKeyOption := ssh.PublicKeyAuth(func(user string, key ssh.PublicKey) bool {
+		return true // allow all keys, or use ssh.KeysEqual() to compare against known keys
 	})
 
-	log.Println("starting ssh server on port: 2222")
-	log.Fatal(ssh.ListenAndServe(":2222", nil, publicKeyHandler))
+	log.Println("starting ssh server on port 2222...")
+	log.Fatal(ssh.ListenAndServe(":2222", nil, publicKeyOption))
 }
