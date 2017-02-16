1e7d2d37c7481d72777fb319cd9f7ff9be7187ef
Jeff Lindsay <progrium@gmail.com>
2017-02-16 15:35:57 -0600

    session: keep extra calculations limited to pty case
    Signed-off-by: Jeff Lindsay <progrium@gmail.com>

--- session.go
+++ session.go
@@ -64,18 +64,19 @@ type session struct {
 }
 
 func (sess *session) Write(p []byte) (n int, err error) {
-	m := len(p)
 	if sess.pty != nil {
+		m := len(p)
 		// normalize \n to \r\n when pty is accepted.
 		// this is a hardcoded shortcut since we don't support terminal modes.
 		p = bytes.Replace(p, []byte{'\n'}, []byte{'\r', '\n'}, -1)
 		p = bytes.Replace(p, []byte{'\r', '\r', '\n'}, []byte{'\r', '\n'}, -1)
+		n, err = sess.Channel.Write(p)
+		if n > m {
+			n = m
+		}
+		return
 	}
-	n, err = sess.Channel.Write(p)
-	if n > m {
-		n = m
-	}
-	return
+	return sess.Channel.Write(p)
 }
 
 func (sess *session) PublicKey() PublicKey {
